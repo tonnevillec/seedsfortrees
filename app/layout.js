dec6977a8a904e2958d4d0599cf10a5236eff580
Cyril <informatique@tibelec.fr>
2023-10-23 11:08:48 +0200

    feat: ajout des textes EN

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,7 @@ import Header from "@/app/Header";
 import Footer from "@/app/Footer";
 export const metadata = {
   title: 'Seeds for trees',
-  description: 'Un geste simple pour renouveler la nature, un arbre à la fois',
+  description: 'A simple gesture to renew nature, one tree at a time. Welcome to "Seeds for Trees," an initiative born from Denman Corporation\'s commitment to preserving our planet and combating climate change. In 2023, Denman Corporation proudly owns forests in North and South Carolina, where nature thrives, and trees are at the heart of our common mission.',
 }
 
 export default function RootLayout({ children }) {
